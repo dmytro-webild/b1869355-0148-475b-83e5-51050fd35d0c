47a840cd633f4450bc50c35e55f6e42a7a23db1b
bender <kudin.dima@gmail.com>
2026-04-28 21:39:55 +0000

    Update src/app/about/page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,7 +12,7 @@ export default function AboutPage() {
   return (
     <ThemeProvider
         defaultButtonVariant="bounce-effect"
-        defaultTextAnimation="reveal-blur"
+        defaultTextAnimation="entrance-slide"
         borderRadius="soft"
         contentWidth="mediumLarge"
         sizing="mediumLargeSizeMediumTitles"
